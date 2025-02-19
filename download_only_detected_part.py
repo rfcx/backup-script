412f44544961e8c69238bcb618ea18cb5ecaabbd
TooSeriuz <frongs@rfcx.org>
2025-02-19 17:21:41 +0700

    Re-structure recording folders

--- download_only_detected_part.py
+++ download_only_detected_part.py
@@ -27,7 +27,7 @@ def trim_audio(src, dest, start, end, ext):
   trimmed_audio.export(dest, format=ext)  # or the desired format
 
 
-def fetch_file(parent_dir, processed_ids, row, detected_recordings):
+def fetch_file(parent_dir, processed_ids, row, detected_recordings, species_name):
     """ Downloads file from a given URL
     :param parent_dir: - default target directory
     :param row: - row from csv file, containing url
@@ -41,37 +41,45 @@ def fetch_file(parent_dir, processed_ids, row, detected_recordings):
         return None
 
     try:
-        # If row contains site data - create a subdirectory for the site
-        if 'site_id' in row:
-            site_id = row['site_id']
-            destination_dir = os.path.join(parent_dir, site_id)
+        # Prepare sub-directories by following format
+        # {species_name}/{present or absent}/{site_id}
+        for det_rec in detected_recordings[row['recording_id']]:
+          spc_name = species_name[det_rec['species_id']]
+          validated = 'present' if det_rec['validated'] == '1' else 'absent'
+          if 'site_id' in row:
+              site_id = row['site_id']
+              destination_dir = os.path.join(parent_dir, spc_name, validated, site_id)
+              if not os.path.exists(destination_dir):
+                try:
+                  os.makedirs(destination_dir)
+                except Exception:
+                  # just ignore in case it is trying to create same folder name due to thread overlap
+                  pass
 
-        if url:
-            file_path = Path(urlparse(url).path)
-            filename = file_path.name
+          if url:
+              file_path = Path(urlparse(url).path)
+              filename = file_path.name
 
-            # Generate filename for recordings
-            date = datetime.strptime(row['datetime'], '%m/%d/%Y %H:%M:%S') if row['datetime'] else None
-            timestamp = date.strftime(TIMESTAMP_FORMAT)
-            name, extension = os.path.splitext(file_path)
-            filename_without_ex = f'{timestamp}-{item_id}'
-            filename = f'{timestamp}-{item_id}{extension}' if timestamp and extension else file_path.name
+              # Generate filename for recordings
+              date = datetime.strptime(row['datetime'], '%m/%d/%Y %H:%M:%S') if row['datetime'] else None
+              timestamp = date.strftime(TIMESTAMP_FORMAT)
+              name, extension = os.path.splitext(file_path)
+              filename_without_ex = f'{timestamp}-{item_id}'
+              filename = f'{timestamp}-{item_id}{extension}' if timestamp and extension else file_path.name
 
-            # Download file
-            target = f'{destination_dir}/{filename}'
-            r = requests.get(url)
-            with open(target, 'wb') as f:
-                f.write(r.content)
-                
-            # Trim only detect species
-            coordinates = detected_recordings[row['recording_id']]
-            for coordinate in coordinates:
-              x1 = float(coordinate['x1'])
-              x2 = float(coordinate['x2'])
+              # Download file
+              target = f'{parent_dir}/{filename}'
+              r = requests.get(url)
+              with open(target, 'wb') as f:
+                  f.write(r.content)
+                  
+              # Trim only detect species
+              x1 = float(det_rec['x1'])
+              x2 = float(det_rec['x2'])
               new_target = f'{destination_dir}/{filename_without_ex}_{x1}_{x2}{extension}'
               trim_audio(target, new_target, x1, x2, extension[1:])
-            Path(target).unlink()
-            return item_id
+        Path(target).unlink()
+        return item_id
     except Exception as e:
         print(f'Error downloading file with recording_id {item_id}: ', e)
         return item_id
@@ -79,9 +87,6 @@ def fetch_file(parent_dir, processed_ids, row, detected_recordings):
 
 if __name__ == '__main__':
     try:
-        # Get all recordings csv files in directory; exclude download tracking files
-        files = filter(lambda x: x.endswith(".csv") and x.startswith("recordings"), os.listdir())
-        
         # Read Pattern matching and collect all data to dict
         pms = filter(lambda x: x.startswith("pattern_matching_rois"), os.listdir())
         detected_recordings = {}
@@ -94,17 +99,32 @@ if __name__ == '__main__':
                     detected_recordings[row['recording_id']] = []
                     detected_recordings[row['recording_id']].append({
                       'x1': row['x1'],
-                      'x2': row['x2']
+                      'x2': row['x2'],
+                      'species_id': row['species_id'],
+                      'validated': row['validated']
                     })
                   else:
                     detected_recordings[row['recording_id']].append({
                       'x1': row['x1'],
-                      'x2': row['x2']
+                      'x2': row['x2'],
+                      'species_id': row['species_id'],
+                      'validated': row['validated']
                     })
+        
+        # Read Species and collect all data to dict
+        species = filter(lambda x: x.startswith("species"), os.listdir())
+        species_name = {}
+        for spc in species:
+          with open(spc, mode='r') as file:
+            content = csv.DictReader(file)
+            rows = list(content)
+            for row in rows:
+              species_name[row['species_id']] = row['scientific_name'].replace(' ', '_')
 
-        # Read files
-        for filename in files:
-            with open(filename, mode='r') as file:
+        # Get all recordings csv files in directory; exclude download tracking files
+        recs = filter(lambda x: x.endswith(".csv") and x.startswith("recordings"), os.listdir())
+        for rec in recs:
+            with open(rec, mode='r') as file:
                 start = time.time()
                 content = csv.DictReader(file)
                 rows = list(content)
@@ -113,19 +133,19 @@ if __name__ == '__main__':
                     # File doesn't have url column - nothing to download
                     continue
                 else:
-                    print(f'[{filename}]: Extracting data from {filename}...')
+                    print(f'[{rec}]: Extracting data from {rec}...')
                     total_count = len(rows)
-                    print(f'[{filename}]: Total items - {total_count}')
+                    print(f'[{rec}]: Total items - {total_count}')
 
                     # Create new directory
                     folder_name = 'detected_recordings'
                     parent_dir = os.path.join(os.getcwd(), folder_name)
                     if not os.path.exists(parent_dir):
-                        print(f'[{filename}]: Creating directory...')
+                        print(f'[{rec}]: Creating directory...')
                         os.makedirs(parent_dir)
 
                     # Get downloaded ids
-                    tracking_file_name = f'{Path(filename).stem}.downloaded.txt'
+                    tracking_file_name = f'{Path(rec).stem}.downloaded.txt'
                     download_interrupted = os.path.exists(tracking_file_name)
                     processed_ids = []
                     if download_interrupted:
@@ -133,31 +153,19 @@ if __name__ == '__main__':
                             processed_ids = list(filter(lambda x: x != '', tracking_file.read().split(';')))
                         processed_count = len(processed_ids)
 
-                        print(f'[{filename}]: Processed items from previous run(s): {processed_count}')
+                        print(f'[{rec}]: Processed items from previous run(s): {processed_count}')
                         if processed_count == total_count:
-                            print(f'[{filename}]: File(s) already downloaded.')
+                            print(f'[{rec}]: File(s) already downloaded.')
                             continue
-
-                    # Create subdirectories
-                    if 'site_id' in content.fieldnames:
-                        site_ids = set([row['site_id'] for row in rows if row['recording_id'] in detected_recordings.keys()])
-                        print(f'[{filename}]: Creating subdirectories for sites...')
-                        for site_id in site_ids:
-                            if site_id:
-                                subdir = os.path.join(parent_dir, site_id)
-
-                                # Create subdirectory for site id
-                                if not os.path.exists(subdir):
-                                    os.makedirs(subdir)
 
                     # Prepare function
                     download = functools.partial(fetch_file, parent_dir, processed_ids)
 
                     # Download files
-                    print(f'[{filename}]: Reading data and downloading files from urls...')
+                    print(f'[{rec}]: Reading data and downloading files from urls...')
                     with ThreadPoolExecutor() as executor:
                         # Filter recording url that out of detected
-                        futures = {executor.submit(download, row, detected_recordings): row['url'] for row in rows if row['recording_id'] in detected_recordings.keys()}
+                        futures = {executor.submit(download, row, detected_recordings, species_name): row['url'] for row in rows if row['recording_id'] in detected_recordings.keys()}
 
                         # As results from each thread become available, push them to tracking file
                         with open(tracking_file_name, 'a') as tf:
@@ -169,7 +177,7 @@ if __name__ == '__main__':
 
                     total_time = time.time() - start
                     print(
-                        f'[{filename}]: Download for {filename} finished. Total time (in seconds): {round(total_time, 2)}')
+                        f'[{rec}]: Download for {rec} finished. Total time (in seconds): {round(total_time, 2)}')
     except KeyboardInterrupt:
         print('Script manually interrupted by user.')
         os._exit(0)
